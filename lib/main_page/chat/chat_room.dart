0859627c3e194801227f786f467431655cb3a7af
MuhdDin <mdin2000@gmail.com>
2024-02-19 09:17:09 +0800

    fix; fix stream chat

--- lib/main_page/chat/chat_room.dart
+++ lib/main_page/chat/chat_room.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -29,7 +30,7 @@ class ChatRoom extends ConsumerStatefulWidget {
 
 class _ChatRoomState extends ConsumerState<ChatRoom> {
   TextEditingController chat = TextEditingController();
-  bool toggle = false;
+  final isSendEnabledProvider = StateProvider<bool>((ref) => false);
   @override
   Widget build(BuildContext context) {
     String ownerUid = ref.watch(uidStateProvider);
@@ -100,93 +101,242 @@ class _ChatRoomState extends ConsumerState<ChatRoom> {
     );
   }
 
+  // Widget showChat(String ownerUid) {
+  //   return Expanded(
+  //       child: FutureBuilder(
+  //     future: StoreFirebase().fetchChat(ownerUid, widget.friendUid),
+  //     builder: (context, snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.done) {
+  //         List<Chat> messages = snapshot.data ?? [];
+  //         print("length: ${messages.length}");
+  //         print(" test: ${messages.first}");
+  //         // if (snapshot.connectionState == ConnectionState.waiting &&
+  //         //     messages.isNotEmpty) {
+  //         //   return buildLastMessage(messages.last);
+  //         // }
+  //         if (messages.isEmpty) {
+  //           return Center(
+  //             child: Column(
+  //               mainAxisAlignment: MainAxisAlignment.center,
+  //               children: [
+  //                 Icon(
+  //                   Icons.sunny,
+  //                   size: 50.h,
+  //                   color: AppConst.kGreyLight,
+  //                 ),
+  //                 Text(
+  //                   "Start chatting with your friend",
+  //                   style: appstyle(
+  //                     20.w,
+  //                     AppConst.kLight,
+  //                     FontWeight.w400,
+  //                   ),
+  //                 )
+  //               ],
+  //             ),
+  //           );
+  //         }
+
+  //         // Check if there's a new message in the chat notifier
+  //         if (ref.watch(chatNotifierProvider).friendUid == widget.friendUid) {
+  //           messages.insert(
+  //               0,
+  //               ref.watch(
+  //                   chatNotifierProvider)); // Insert new message at the beginning
+  //         }
+  //         return ListView.builder(
+  //           reverse: true,
+  //           itemCount: messages.length,
+  //           itemBuilder: (context, index) {
+  //             return Column(
+  //               children: [
+  //                 Container(
+  //                   padding: const EdgeInsets.only(
+  //                       left: 14, right: 14, top: 10, bottom: 10),
+  //                   child: Align(
+  //                     alignment: (messages[index].messageType == "receive"
+  //                         ? Alignment.topLeft
+  //                         : Alignment.topRight),
+  //                     child: Container(
+  //                       decoration: BoxDecoration(
+  //                         borderRadius: BorderRadius.circular(20),
+  //                         color: (messages[index].messageType == "receive"
+  //                             ? Colors.grey.shade200
+  //                             : Colors.blue[200]),
+  //                       ),
+  //                       padding: EdgeInsets.all(10.w),
+  //                       child: Text(
+  //                         messages[index].message,
+  //                         style: const TextStyle(fontSize: 15),
+  //                       ),
+  //                     ),
+  //                   ),
+  //                 ),
+  //                 Align(
+  //                   alignment: (messages[index].messageType == "receive"
+  //                       ? Alignment.topLeft
+  //                       : Alignment.topRight),
+  //                   child: Padding(
+  //                     padding: EdgeInsets.only(
+  //                         left: 14.0.w, right: 14.0.w, bottom: 5.h),
+  //                     child: Text(
+  //                       calculateDaysDifference(
+  //                           messages[index].createdAt.toString()),
+  //                       style: appstyle(
+  //                           12.w, AppConst.kGreyLight, FontWeight.w200),
+  //                     ),
+  //                   ),
+  //                 ),
+  //               ],
+  //             );
+  //           },
+  //         );
+  //       } else {
+  //         return Container();
+  //       }
+  //     },
+  //   ));
+  // }
   Widget showChat(String ownerUid) {
     return Expanded(
-      child: FutureBuilder(
-        future: StoreFirebase().fetchChat(ownerUid, widget.friendUid),
+      child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: StoreFirebase().streamChat(ownerUid, widget.friendUid),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            List<Chat> messages = snapshot.data!;
-            return Consumer(builder: (context, ref, child) {
-              Chat newMessage = ref.watch(chatNotifierProvider);
-              if (toggle == true) {
-                messages.insert(0, newMessage);
-                toggle = false;
-              }
-              if (messages.isEmpty) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.sunny,
-                        size: 50.h,
-                        color: AppConst.kGreyLight,
+          print("stream");
+          print("checks:${snapshot.hasData}");
+          print("stream err: ${snapshot.error}");
+          if (snapshot.connectionState == ConnectionState.active) {
+            print("Checking data.... ${snapshot.data!.docs}");
+            List<Chat> messages = snapshot.data!.docs.map((documentSnapshot) {
+              Map<String, dynamic> messageData = documentSnapshot.data();
+              return Chat.fromMap(messageData);
+            }).toList();
+            // List<Chat> messages = snapshot.data!;
+            if (messages.isEmpty) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.sunny,
+                      size: 50.h,
+                      color: AppConst.kGreyLight,
+                    ),
+                    Text(
+                      "Start chatting with your friend",
+                      style: appstyle(
+                        20.w,
+                        AppConst.kLight,
+                        FontWeight.w400,
                       ),
-                      Text(
-                        "Start chatting with your friend",
-                        style: appstyle(
-                          20.w,
-                          AppConst.kLight,
-                          FontWeight.w400,
+                    )
+                  ],
+                ),
+              );
+            }
+            // print("Has Data, ${messages}");
+            return ListView.builder(
+              key: UniqueKey(),
+              reverse: true,
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                print("stream2, ${messages[index].message}");
+                return Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(
+                        left: 14,
+                        right: 14,
+                        top: 10,
+                        bottom: 10,
+                      ),
+                      child: Align(
+                        alignment: (messages[index].messageType == "receive"
+                            ? Alignment.topLeft
+                            : Alignment.topRight),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: (messages[index].messageType == "receive"
+                                ? Colors.grey.shade200
+                                : Colors.blue[200]),
+                          ),
+                          padding: EdgeInsets.all(10.w),
+                          child: Text(
+                            messages[index].message,
+                            style: const TextStyle(fontSize: 15),
+                          ),
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    ),
+                    Align(
+                      alignment: (messages[index].messageType == "receive"
+                          ? Alignment.topLeft
+                          : Alignment.topRight),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: 14.0.w,
+                          right: 14.0.w,
+                          bottom: 5.h,
+                        ),
+                        child: Text(
+                          calculateDaysDifference(
+                              messages[index].createdAt.toString()),
+                          style: appstyle(
+                              12.w, AppConst.kGreyLight, FontWeight.w200),
+                        ),
+                      ),
+                    ),
+                  ],
                 );
-              }
-              return ListView.builder(
-                  reverse: true,
-                  itemCount: messages.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(
-                              left: 14, right: 14, top: 10, bottom: 10),
-                          child: Align(
-                            alignment: (messages[index].messageType == "receive"
-                                ? Alignment.topLeft
-                                : Alignment.topRight),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: (messages[index].messageType == "receive"
-                                    ? Colors.grey.shade200
-                                    : Colors.blue[200]),
-                              ),
-                              padding: EdgeInsets.all(10.w),
-                              child: Text(
-                                messages[index].message,
-                                style: const TextStyle(fontSize: 15),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: (messages[index].messageType == "receive"
-                              ? Alignment.topLeft
-                              : Alignment.topRight),
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                left: 14.0.w, right: 14.0.w, bottom: 5.h),
-                            child: Text(
-                              calculateDaysDifference(
-                                  messages[index].createdAt.toString()),
-                              style: appstyle(
-                                  12.w, AppConst.kGreyLight, FontWeight.w200),
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  });
-            });
+              },
+            );
           } else {
-            return Container();
+            return Container(); // Show loading indicator while data is being fetched
           }
         },
       ),
+    );
+  }
+
+  Widget buildLastMessage(Chat message) {
+    return Column(
+      children: [
+        Container(
+          padding:
+              const EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+          child: Align(
+            alignment: (message.messageType == "receive"
+                ? Alignment.topLeft
+                : Alignment.topRight),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: (message.messageType == "receive"
+                    ? Colors.grey.shade200
+                    : Colors.blue[200]),
+              ),
+              padding: EdgeInsets.all(10.w),
+              child: Text(
+                message.message,
+                style: const TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: (message.messageType == "receive"
+              ? Alignment.topLeft
+              : Alignment.topRight),
+          child: Padding(
+            padding: EdgeInsets.only(left: 14.0.w, right: 14.0.w, bottom: 5.h),
+            child: Text(
+              calculateDaysDifference(message.createdAt.toString()),
+              style: appstyle(12.w, AppConst.kGreyLight, FontWeight.w200),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -203,6 +353,13 @@ class _ChatRoomState extends ConsumerState<ChatRoom> {
               controller: chat,
               obscureText: false,
               color: const Color.fromARGB(255, 121, 119, 119),
+              onChanged: (value) {
+                if (value.trim().isEmpty) {
+                  ref.read(isSendEnabledProvider.notifier).state = false;
+                } else {
+                  ref.read(isSendEnabledProvider.notifier).state = true;
+                }
+              },
               prefixIcon: IconButton(
                 onPressed: () async {},
                 icon: Container(
@@ -218,29 +375,36 @@ class _ChatRoomState extends ConsumerState<ChatRoom> {
                   ),
                 ),
               ),
-              suffixIcon: IconButton(
-                onPressed: () async {
-                  String message = chat.text;
-                  chat.clear();
-                  Chat newMessage = Chat(
-                      friendUid: widget.friendUid,
-                      messageType: 'sender',
-                      createdAt: DateTime.now(),
-                      message: message,
-                      read: false);
-                  toggle = true;
-                  ref
-                      .read(chatNotifierProvider.notifier)
-                      .updateChat(newMessage);
+              suffixIcon: Consumer(builder: (context, ref, child) {
+                bool enableSend = ref.watch(isSendEnabledProvider);
+                print(enableSend);
+                return IconButton(
+                  onPressed: enableSend
+                      ? () async {
+                          String message = chat.text;
+                          chat.clear();
+                          ref.read(isSendEnabledProvider.notifier).state =
+                              false;
+                          Chat newMessage = Chat(
+                              friendUid: widget.friendUid,
+                              messageType: 'sender',
+                              createdAt: DateTime.now(),
+                              message: message,
+                              read: false);
+                          ref
+                              .read(chatNotifierProvider.notifier)
+                              .updateChat(newMessage);
 
-                  await StoreFirebase()
-                      .storeChat(ownerUid, widget.friendUid, message);
-                },
-                icon: const Icon(
-                  Icons.arrow_forward_ios,
-                  color: AppConst.kLight,
-                ),
-              ),
+                          await StoreFirebase()
+                              .storeChat(ownerUid, widget.friendUid, message);
+                        }
+                      : null,
+                  icon: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: AppConst.kLight,
+                  ),
+                );
+              }),
             ),
           ),
         ],
